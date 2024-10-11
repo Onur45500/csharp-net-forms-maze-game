417f94552adaa7f0087128ba052f42d92d67c3c7
Onur45500 <onur.akmese@viacesi.fr>
2024-10-11 16:45:06 +0200

    Set up the form and the maze

---
+++ csharp-net-forms-maze-game/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace csharp_net_forms_maze_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BorderStyle = BorderStyle.Fixed3D;
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(0, -1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(785, 555);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.BackColor = SystemColors.Desktop;
+            label1.Location = new Point(-2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(671, 264);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.BackColor = SystemColors.Desktop;
+            label2.Location = new Point(-2, 298);
+            label2.Name = "label2";
+            label2.Size = new Size(707, 255);
+            label2.TabIndex = 1;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.BackColor = SystemColors.Desktop;
+            label3.Location = new Point(704, -2);
+            label3.Name = "label3";
+            label3.Size = new Size(79, 555);
+            label3.TabIndex = 2;
+            label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(782, 553);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Label label3;
+        private Label label2;
+    }
+}
